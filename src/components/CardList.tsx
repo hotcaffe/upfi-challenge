af8230db6f430b69577e97a8233ccfda1d7f4ddf
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-08-02 06:44:31 -0300

    Fetching fauna data

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { Grid, GridItem, VStack, Image, SimpleGrid, Text, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -25,7 +25,25 @@ export function CardList({ cards }: CardsProps): JSX.Element {
   return (
     <>
       {/* TODO CARD GRID */}
+      <Grid templateColumns="repeat(3, 1fr)" gap='40px' >
+        {cards.map(card => (
+          <GridItem key={card.id} w="293px" h="290px" backgroundColor='gray.800' borderRadius='6px'>
+            <Image src={card.url} borderRadius='6px 6px 0 0'/>
+            <VStack align='flex-start' justify='center' h='100px' mx='25px' spacing={1}>
+              <Text fontWeight='bold' fontSize='2xl' color='gray.50'>{card.title}</Text>
+              <Text color='gray.100'>{card.description}</Text>
+            </VStack>
+          </GridItem>
+        ))}
+        {/* <GridItem w="293px" h="290px" bg="blue.500" borderBox='6px'>
 
+        </GridItem>
+        <GridItem w="293px" h="290px" bg="blue.500"></GridItem>
+        <GridItem w="293px" h="290px" bg="blue.500"></GridItem>
+        <GridItem w="293px" h="290px" bg="blue.500"></GridItem>
+        <GridItem w="293px" h="290px" bg="blue.500"></GridItem>
+        <GridItem w="293px" h="290px" bg="blue.500"></GridItem> */}
+      </Grid>
       {/* TODO MODALVIEWIMAGE */}
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Box } from '@chakra-ui/react';
+import { Button, Box, Text } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
@@ -18,18 +18,27 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    ({pageParam = 1}) => api.get(`/api/images?after=${pageParam}`),
+    {
+      getNextPageParam: (lastPage) => lastPage?.data.after
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const pages = data?.pages.map(page => {
+      return page.data.data
+    })
+
+    return pages?.flat()
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if(isLoading) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if(isError) {
+    return <Error />
+  }
 
   return (
     <>
@@ -37,7 +46,8 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && <Button mt='40px' onClick={() => fetchNextPage()}>Carregar mais</Button>}
+        {isFetchingNextPage && <Text fontSize="sm" mt="5px">Carregando...</Text>}
       </Box>
     </>
   );
