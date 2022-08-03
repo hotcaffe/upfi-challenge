8e241d97bc5fdfaa18dbd58524d1e9e5767323c8
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-08-03 06:29:35 -0300

    View image component

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -21,12 +21,15 @@ export function ModalViewImage({
 }: ModalViewImageProps): JSX.Element {
 
   return (
-    <Modal isOpen={isOpen} isCentered onClose={() => onClose()} size="4xl">
+    <Modal isOpen={isOpen} isCentered onClose={() => onClose()} size="5xl">
       <ModalOverlay/>
-      <ModalContent bgColor='pGray.900'>
-        <ModalBody maxWidth={900} maxHeight={600}>
-          <Image src={imgUrl}/>
+      <ModalContent bgColor='pGray.900' maxWidth={900}>
+        <ModalBody p={0} m={0}>
+          <Image src={imgUrl} maxWidth={900} maxHeight={600}/>
         </ModalBody>
+        <ModalFooter h="32px" justifyContent='flex-start'>
+          <Link href={imgUrl} isExternal color="pGray.50" fontSize="sm">Abrir original</Link>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   )
